e95cd734c7e37072c8f2dac8e3607c2dc1b959fc
SoroinDevs <takashiminamoto14@gmail.com>
2024-07-24 22:40:08 -0400

    initial commit

---
+++ src/mock/profile.js
@@ -0,0 +1,23 @@
+export const SOCIAL_LINKS = [
+  {
+    link: 'https://x.com/Javier1998826',
+    icon: 'akar-icons:twitter-fill',
+  },
+  {
+    link: 'https://github.com/StainerHenry',
+    icon: 'akar-icons:github-fill',
+  },
+  {
+    link: 'https://t.me/alwaysno7',
+    icon: 'akar-icons:telegram-fill',
+  },
+];
+
+export const HERO_TITLES = ['Javier Martinez', 'Full-stack & Blockchain Engineer', 'AI Enthusiast'];
+
+export const aboutParagraph =
+  'Highly motivated and enthusiastic Full Stack & Blockchain Engineer with experience in designing, developing and maintaining Web2 & Web3 platforms using technologies such as Solidity, Rust, FunC, Tact, Huskell, React, Next.js, React-Native, Vue, ';
+
+export const contactEmail = 'javiermartinez98826@gmail.com';
+
+export const documentTitle = 'Hello I am Javier Martinez | Full Stack & Blockchain Engineer | AI Enthusiast';
